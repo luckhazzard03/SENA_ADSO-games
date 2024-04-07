d2b48eb076cd27e136535a134a88252dd7cd5837
Angel Eduardo Medina Rojas <124850635+luckhazzard03@users.noreply.github.com>
2024-04-07 02:24:36 -0500

    SE AJUSTA CRONOMETRO

--- SENA_ADSO-games/games_memory/assets/js/Chronometer.js
+++ SENA_ADSO-games/games_memory/assets/js/Chronometer.js
@@ -4,10 +4,9 @@ Date:13/04/2024
 Description: 
 */
 class Chronometer {
-
   constructor(chronometerId, speed, maxMilliseconds) {
     this.objChronometer = document.getElementById(chronometerId);
-    this.getElementsLabel = this.objChronometer.querySelectorAll('label');
+    this.getElementsLabel = this.objChronometer.querySelectorAll("label");
     this.conT = 0;
     this.seconds = "00";
     this.minutes = "00";
@@ -22,12 +21,21 @@ class Chronometer {
 
   startChronometer() {
     this.intervalID = setInterval(() => {
-      this.seconds = this.secondsAux.toString().length == 1 ? "0" + this.secondsAux.toString() : this.secondsAux;
+      this.seconds =
+        this.secondsAux.toString().length == 1
+          ? "0" + this.secondsAux.toString()
+          : this.secondsAux;
       this.getElementsLabel[2].innerHTML = this.seconds;
       this.secondsAux++;
-      this.minutes = this.minutesAux.toString().length == 1 ? "0" + this.minutesAux.toString() : this.minutesAux;
+      this.minutes =
+        this.minutesAux.toString().length == 1
+          ? "0" + this.minutesAux.toString()
+          : this.minutesAux;
       this.getElementsLabel[1].innerHTML = this.minutes;
-      this.hourd = this.hourdAux.toString().length == 1 ? "0" + this.hourdAux.toString() : this.hourdAux;
+      this.hourd =
+        this.hourdAux.toString().length == 1
+          ? "0" + this.hourdAux.toString()
+          : this.hourdAux;
       this.getElementsLabel[0].innerHTML = this.hourd;
 
       if (this.secondsAux == 60) {
@@ -43,7 +51,6 @@ class Chronometer {
         this.clearChronometer();
       }
       this.conT++;
-
     }, this.speed);
   }
   clearChronometer() {
@@ -51,7 +58,6 @@ class Chronometer {
     this.getElementsLabel[0].innerHTML = "00";
     this.getElementsLabel[1].innerHTML = "00";
     this.getElementsLabel[2].innerHTML = "00";
-
   }
 
   getSeconds() {
